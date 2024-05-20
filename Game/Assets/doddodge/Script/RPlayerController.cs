5201b35d609c9f295d60e8847794f7d2a74a7aa3
savom <56549021+savom@users.noreply.github.com>
2024-05-20 10:15:32 +0900

    plane - player
    플레이어까지 구현

---
+++ Game/Assets/doddodge/Script/RPlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 8.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(-xSpeed, 0f, -zSpeed);
+        rb.velocity = newVelocity;
+
+
+
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
